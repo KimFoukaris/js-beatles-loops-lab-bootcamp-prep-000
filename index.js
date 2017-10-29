ce0f9e10d51cd6369b322976dacca05755f6d4fd
Kim Foukaris <kimfoukaris@outlook.com>
2017-10-29 15:07:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
 var array = []
 let i = 0
-while i < facts.length {
+while (i < facts.length) {
   array[i] = facts[i] + "!!!"
   i=i+1
 }
